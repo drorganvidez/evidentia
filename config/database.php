e99b5c82d619c7cd43903f2610c4559818784656
David Romero <davromorg@alum.us.es>
2020-03-03 17:35:46 +0100

    Actualizada configuración
    Se ha actualizado la configuración para que funcione con Laravel Homestead

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '33060'),
+            'database' => env('DB_DATABASE', 'homestead'),
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', 'secret'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -62,6 +62,16 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
+        'instance' => array(
+            'driver'   => 'mysql',
+            'host'     => '',
+            'port'     => '',
+            'database' => '',
+            'username' => '',
+            'password' => '',
+            'prefix'   => '',
+        ),
 
         'pgsql' => [
             'driver' => 'pgsql',
