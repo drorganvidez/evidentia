ae8006070f7844feb0c2b8e45731799f0703fc82
David Romero <davromorg@alum.us.es>
2020-07-30 12:22:29 +0200

    Cambio en install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 cd homestead
 vagrant box add laravel/homestead
 vagrant up
-echo "192.168.10.10 evidentia.test" >> /etc/hosts
+sudo echo "192.168.10.10 evidentia.test" >> /etc/hosts
 vagrant ssh -c 'cd laravel; composer install; php artisan evidentia:start; php artisan evidentia:createinstance'
