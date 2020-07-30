d26ac6798710f9b48c6925975351ca35e3879b9b
David Romero <davromorg@alum.us.es>
2020-07-30 18:44:16 +0200

    impr: Mejora de eficiencia en la instalación
    Ahora el host virtual es lo primero que se crea, para no tener que estar pendiente de la consola durante la instalación

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
+echo 192.168.10.10 evidentia.test >> %WINDIR%\System32\Drivers\Etc\Hosts
 cd homestead
 vagrant box add laravel/homestead
 vagrant up
-echo 192.168.10.10 evidentia.test >> %WINDIR%\System32\Drivers\Etc\Hosts
 vagrant ssh -c 'cd laravel; composer install; php artisan evidentia:start; php artisan evidentia:createinstance'
