0143f03e3e1054a2088ecf8d8051eac963dd806b
David Romero <davromorg@alum.us.es>
2020-09-18 12:14:47 +0200

    fix: Corregido pequeño bug en el menú admin
    No se marcaba el elemento del menú en las subsecciones de las instancias

--- resources/views/components/menus/menuadmin.blade.php
+++ resources/views/components/menus/menuadmin.blade.php
@@ -3,7 +3,7 @@
 
         <x-li route="admin.instance.create" icon='fas fa-box' name="Crear instancia"/>
 
-        <x-li route="admin.instance.manage" icon='fas fa-boxes' name="Gestionar instancias"/>
+        <x-li route="admin.instance.manage" secondaries="admin.instance.manage.edit,admin.instance.manage.delete" icon='fas fa-boxes' name="Gestionar instancias"/>
 
         <x-lilogout/>
 
