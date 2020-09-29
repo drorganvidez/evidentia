5f671ad1ca3efc02b8415e64695ec5c713deb4fb
David Romero <davromorg@alum.us.es>
2020-09-29 19:01:40 +0200

    ci: Despliegue erróneo a propósito

--- tests/Feature/InstanceTest.php
+++ tests/Feature/InstanceTest.php
@@ -19,7 +19,7 @@ class InstanceTest extends TestCase
     public function testSettingUp() :void {
 
         DB::connection()->getPdo()->exec("DROP DATABASE IF EXISTS `homestead`;");
-        DB::connection()->getPdo()->exec("DROP DATABASE IF EXISTS `basetest`;");
+        //DB::connection()->getPdo()->exec("DROP DATABASE IF EXISTS `basetest`;");
         DB::connection()->getPdo()->exec("CREATE DATABASE IF NOT EXISTS `homestead`");
         DB::connection()->getPdo()->exec("ALTER SCHEMA `homestead`  DEFAULT CHARACTER SET utf8mb4  DEFAULT COLLATE utf8mb4_unicode_ci");
         exec("php artisan migrate");
