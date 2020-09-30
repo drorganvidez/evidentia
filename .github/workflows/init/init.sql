12f8a134ba8bdae439c01a1d7f02e8729f24f28b
David Romero <davromorg@alum.us.es>
2020-09-30 14:25:25 +0200

    ci: init.sql erróneo a propósito

--- .github/workflows/init/init.sql
+++ .github/workflows/init/init.sql
@@ -1,5 +1,3 @@
-GRANT USAGE ON *.* TO 'homestead'@localhost IDENTIFIED BY 'secret';
-GRANT USAGE ON *.* TO 'homestead'@'%' IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON *.*  TO 'homestead'@localhost identified by 'secret';
-GRANT ALL PRIVILEGES ON *.*  TO 'homestead'@'%' identified by 'secret';
-FLUSH PRIVILEGES;
+GRANT ALL ON *.*  TO 'homestead'@localhost identified by 'secret';
+GRANT ALL ON *.*  TO 'homestead'@'%' identified by 'secret';
+FLUSH PRIVILE
