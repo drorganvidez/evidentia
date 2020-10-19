3396b899e15b9c56e0a640c5f65579014acc365d
David Romero <davromorg@alum.us.es>
2020-10-19 17:55:08 +0200

    fix: Mejorados y corregidos los archivos de instalación
    Existían una serie de errores a la hora de instalar Evidentia en Windows. Las soluciones se han incluido como comandos para agilizar el proceso

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 @echo off
 cd homestead
-vagrant box add laravel/homestead
+set VAGRANT_HOME=C:\HashiCorp\Vagrant
+vagrant box remove laravel/homestead --all
+vagrant box add laravel/homestead -c
 vagrant up
 vagrant ssh -c 'cd laravel; composer install; php artisan evidentia:start; php artisan evidentia:createinstance'
