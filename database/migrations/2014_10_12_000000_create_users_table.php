bb8cd390a6f1ea4cc390ee6d74b51b69b7804b4f
David Romero <davromorg@alum.us.es>
2020-11-13 19:58:35 +0100

    fix: Faltaba un campo nuevo
    Se ha añadido el campo administrator

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('administrator')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
