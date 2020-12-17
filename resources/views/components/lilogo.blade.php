bda6d44cc225df24f846fbaffd8db73ddd374567
David Romero <davromorg@alum.us.es>
2020-12-17 12:34:50 +0100

    Versión 1.3

--- resources/views/components/lilogo.blade.php
+++ resources/views/components/lilogo.blade.php
@@ -1,3 +1,3 @@
 <a href="/" class="brand-link">
-    <span class="brand-text font-weight-light" style="padding-left: 15px;">evidentia.<b>cloud</b><sup>v1.2</sup></span>
+    <span class="brand-text font-weight-light" style="padding-left: 15px;">evidentia.<b>cloud</b><sup>v1.3</sup></span>
 </a>
