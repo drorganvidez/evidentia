7361a233eca019711b691c59c583bf01d38a2e83
David Romero <davromorg@alum.us.es>
2021-10-29 13:50:06 +0200

    fix: Actualizada instalación de Homestead

--- install_homestead.bat
+++ install_homestead.bat
@@ -1,8 +1,25 @@
 @echo off
 git submodule update --init --recursive
 cd homestead
-git pull origin master
+git pull origin main
 vagrant box remove laravel/homestead --all
 vagrant box add laravel/homestead -c
 vagrant up
 vagrant ssh -c 'cd evidentia; composer install; php artisan evidentia:start_homestead; php artisan evidentia:createinstance'
+
+echo
+echo WELCOME TO
+echo .########.##.....##.####.########..########.##....##.########.####....###...;
+echo .##.......##.....##..##..##.....##.##.......###...##....##.....##....##.##..;
+echo .##.......##.....##..##..##.....##.##.......####..##....##.....##...##...##.;
+echo .######...##.....##..##..##.....##.######...##.##.##....##.....##..##.....##;
+echo .##........##...##...##..##.....##.##.......##..####....##.....##..#########;
+echo .##.........##.##....##..##.....##.##.......##...###....##.....##..##.....##;
+echo .########....###....####.########..########.##....##....##....####.##.....##;
+echo
+
+echo Open in a new browser:
+echo http://localhost
+
+echo
+echo The installation has been completed successfully. Enjoy!
