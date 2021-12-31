474e68243f4aa8b5ef646fe2440ddc8972bc234e
miguesnow <miguenieva2000@gmail.com>
2021-12-31 12:18:25 +0100

    Validación

--- resources/views/certificate/create.blade.php
+++ resources/views/certificate/create.blade.php
@@ -12,12 +12,11 @@
 @endsection
 
 @section('content')
-
-    <form method="GET" enctype="multipart/form-data">
+    
+    <form method="GET" enctype="multipart/form-data" )>
 
         <x-id :id="$evidence->id ?? ''" :edit="$edit ?? ''"/>
 
-        <!-- <input type="hidden" name="removed_files" id="removed_files"/> -->
 
 
         <div class="row">
@@ -26,16 +25,46 @@
 
                 <div class="card shadow-sm">
 
+                <div class="widget-user-header text-white" style="background: url(http://localhost/dist/img/abstract.jpg); height: 50px" bis_skin_checked="1">
+
+                                    <h5 class="widget-user-desc text-left" style="margin-bottom: 0px; margin-top: 12px; margin-left: 7px;">
+                                        <i class="fas fa-pencil-alt"></i>
+                                        &nbsp;&nbsp;
+                                        Formulario
+                                    </h5>
+
+                                </div>
+
                     <div class="card-body">
 
                         <div class="form-row">
 
-                            <x-input col="5" attr="nombreDiploma" :value="$evidence->title ?? ''" label="Nombre Diploma" description="Escribe un título que describa el diploma que vas a generar (mínimo 5 caracteres)"/>
-                            <x-input col="5" attr="name" :value="$evidence->title ?? ''" label="Nombre del premiado" description="Escribe el nombre de la persona premiada"/>
-                            <x-input col="5" attr="mailto" :value="$evidence->title ?? ''" label="Correo electrónico del premiado" description="Escribe el mail de la persona premiada"/>
-                            <x-input col="5" attr="course" :value="$evidence->title ?? ''" label="Curso realizado" description="Escribe el mail de la persona premiada"/>
-                            <!-- <x-selectfield col="5" attr="diplomaGenerar" slot="@foreach($certificates as $c) <option value='$c->html ?? ""'> {{$c->title ?? ''}} </option>@endforeach" label="Diploma a generar" description="Escribe el mail de la persona premiada" id="certificates"/>
- -->                        <div class="form-group col-md-5">
+                            <x-input col="5" attr="nombreDiploma" :value="$evidence->title ?? ''" label="Nombre del Diploma" description="Nombre del archivo pdf que contendrá el diploma"/>
+                            <div class="form-group col-md-5">
+                                <label for="name">Nombre del premiado</label>
+                                <input id="name" type="ctype_alpha" class="form-control" placeholder="Jose Luis Gomez Parejo" name="name"   description="Nombre de la persona premiada" required>
+                                <small class="form-text text-muted">Nombre del premiado</small>
+                                @error("name")
+                                <span class="invalid-feedback d-block" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div> 
+
+                            <div class="form-group col-md-5">
+                                <label for="mailto">Mail del Premiado/a</label>
+                                <input id="mailto" type="email" class="form-control" placeholder="joseluis32@gmail.com" name="mailto"   description="Correo electrónico de la persona premiada" required>
+                                <small class="form-text text-muted">Mail del Premiado</small>
+                                @error("name")
+                                <span class="invalid-feedback d-block" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <x-input col="5" attr="course" :value="$evidence->title ?? ''" label="Curso realizado" description="Nombre del curso que ha realizado"/>
+                            
+                            <div class="form-group col-md-5">
                                 <label>Seleccionar diploma a generar</label>
                                 <select id="certificates" name="diplomaGenerar" class="selectpicker form-control">
                                     @foreach($certificates as $c)
@@ -43,47 +72,46 @@
                                     @endforeach
                                 </select>
                                 <small class="form-text text-muted">Selecciona el diploma que quieres generar con los datos del formulario.</small>
-                                <!-- @error('users')
-                                <span class="invalid-feedback d-block" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror -->
-                            </div>
-                            <x-input col="5" attr="score" :value="$evidence->title ?? ''" label="Puntuación" description="Escribe el mail de la persona premiada"/>
-                            <x-input col="5" attr="date" :value="$evidence->title ?? ''" label="Fecha (10/2/12)" description="Escribe el mail de la persona premiada"/>
 
-                            <!-- <div class="form-group col-md-2">
-                                <label for="hours">Nombre de la persona</label>
-                                <input id="" type="number" min="0" max="99" class="form-control" placeholder="" name="hours" value="{{\Time::complex_shape_hours($evidence->hours ?? '')}}" autocomplete="hours" autofocus="" step="0.01">
-                                <small class="form-text text-muted">Enteros o decimales</small>
-                                @error("hours")
-                                <span class="invalid-feedback d-block" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div> -->
-
-                            <!-- <div class="form-group col-md-2">
-                                <label for="minutes">Minutos</label>
-                                <input id="" type="number" min="0" max="60" class="form-control" placeholder="" name="minutes" value="{{\Time::complex_shape_minutes($evidence->hours ?? '') }}" autocomplete="minutes" autofocus="">
-                                <small class="form-text text-muted">Enteros</small>
-                                @error("minutes")
-                                <span class="invalid-feedback d-block" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div> -->
-
+                            </div>                            
                             
+                            <div class="form-group col-md-5">
+                                <label for="score">Puntuación</label>
+                                <input id="score" type="number" min="0" max="10" class="form-control" placeholder="1-10" name="score" value="$evidence->score ?? ''"  step="1" description="Puntuación obtenida por el ganador en el curso" required>
+                                <small class="form-text text-muted">Enteros</small>
+                                @error("score")
+                                <span class="invalid-feedback d-block" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div> 
 
-                            <!-- <x-textarea col="12" attr="description" :value="$evidence->description ?? ''"
-                                        label="Descripción de la evidencia"
-                                        description="Escribe una descripción concisa de tu evidencia (entre 10 y 20000 caracteres)."
-                            /> -->
+                            <div class="form-group col-md-5">
+                                                <label for="date">Fecha</label>
+                                                <input id="date" type="date"
+                                                       class="form-control @error('date') is-invalid @enderror" name="date"
+                                                       @if(old('date'))
+                                                       value="{{old('date')}}"
+                                                       @else
+                                                       value=""
+                                                       @endif
+                                                       autofocus
+                                                       placeholder="02/10/2021"
+                                                       required>
+                                                <small class="form-text text-muted">Fecha de generación del diploma.
+                                                </small>
+
+                                                @error('date')
+                                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                                @enderror
+                                            </div>
+
 
 
                             <div class="form-group col-md-4">
-                                <button type="button"  class="btn btn-primary btn-block" data-toggle="modal" data-target="#modal-default"><i class="fas fa-external-link-square-alt"></i> &nbsp;Publicar evidencia</button>
+                                <button type="button"  formaction="http://127.0.0.1:5000/diploma" class="btn btn-primary btn-block" data-toggle="modal" data-target="#modal-default" style = "margin-top:60px; margin-left: 120px"><i class="fas fa-external-link-square-alt"></i> &nbsp;Generar diploma</button>
                             </div>
 
                             <div class="modal fade" id="modal-default">
@@ -92,32 +120,34 @@
                                         <div class="modal-header">
                                             <h4 class="modal-title">Generar diploma</h4>
                                         </div>
-                                        <div class="modal-body">
+                                       <!-- <div class="modal-body">
                                             <p>Cuando se genera un diploma, este se envía al email de la persona premiada
                                                 por lo que
                                                 <b>no podrá ser eliminado.</b></p>
                                             <p>¿Deseas continuar?</p>
-                                        </div>
+                                        </div> -->
                                         <div class="modal-footer justify-content-between">
-                                            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                                            <button type="submit" formaction="http://127.0.0.1:5000/diploma" class="btn btn-primary" data-toggle="modal" data-target="#modal-default"><i class="fas fa-external-link-square-alt"></i> &nbsp;Sí, publicar evidencia</button>
+                                           <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button> -->
+                                            <button type="submit" formaction="http://127.0.0.1:5000/diploma" class="btn btn-primary" data-toggle="modal" data-target="#modal-default"><i class="fas fa-external-link-square-alt"></i> &nbsp;Sí, generar diploma</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
+ 
 
                         </div>
 
                     </div>
 
                 </div>
-
+                
             </div>
-
+        
+        
         </div>
-
+        
     </form>
+
 
     @section('scripts')
 
@@ -126,5 +156,3 @@
     @endsection
 
 @endsection
-
-
