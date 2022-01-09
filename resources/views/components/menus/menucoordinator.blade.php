0f587582ac1ba5cee1494e40006435328f99fade
Antonio Campuzano Martinez <62079678+xNegis@users.noreply.github.com>
2022-01-09 13:15:04 +0100

    probando integracion

--- resources/views/components/menus/menucoordinator.blade.php
+++ resources/views/components/menus/menucoordinator.blade.php
@@ -3,7 +3,7 @@
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-            <li class="nav-header">COMITÉ</li>
+            <li class="nav-header">COMITE</li>
             <x-li route="coordinator.evidence.list.all"
                   secondaries="coordinator.evidence.view,coordinator.evidence.list.all,coordinator.evidence.list.pending,coordinator.evidence.list.accepted,coordinator.evidence.list.rejected"
                   icon='fas fa-clipboard-check' name="Gestionar evidencias"/>
