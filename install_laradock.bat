57925a56944c693fd5ab1866da0cfa16223d7967
David Romero Organvídez <drorganvidez@icloud.com>
2022-05-30 10:51:32 +0200

    Update install_laradock.bat

--- install_laradock.bat
+++ install_laradock.bat
@@ -1,16 +1,20 @@
 @echo off
 git submodule update --init --recursive
-COPY .env.laradock laradock/.env
-COPY createdb.sql laradock/mysql/docker-entrypoint-initdb.d/createdb.sql
+COPY .env.laradock laradock\.env
+COPY createdb.sql laradock\mysql\docker-entrypoint-initdb.d\createdb.sql
 cd laradock
 git pull origin master
 docker-compose up -d nginx mysql phpmyadmin redis workspace
-docker exec laradock_workspace_1 rm -f composer.lock
-docker exec laradock_workspace_1 composer install
-docker exec laradock_workspace_1 php artisan evidentia:start_docker
-docker exec laradock_workspace_1 php artisan evidentia:createinstance
-docker exec laradock_workspace_1 php artisan key:generate
-docker exec laradock_workspace_1 php artisan config:cache
+docker exec laradock-php-fpm-1 chown -R www-data:www-data /var/www/storage
+docker exec laradock-workspace-1 rm -f composer.lock
+docker exec laradock-workspace-1 composer install
+docker exec laradock-workspace-1 npm install
+docker exec laradock-workspace-1 npx mix
+docker exec laradock-workspace-1 php artisan evidentia:start docker
+docker exec laradock-workspace-1 php artisan evidentia:start docker
+docker exec laradock-workspace-1 php artisan evidentia:instance
+docker exec laradock-workspace-1 php artisan key:generate
+docker exec laradock-workspace-1 php artisan config:cache
 
 echo
 echo WELCOME TO
@@ -28,3 +32,5 @@ echo http://localhost
 
 echo
 echo The installation has been completed successfully. Enjoy!
+
+pause
