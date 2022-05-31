055f38fb0fa91329ed2d74c928d36cc7b3855183
David Romero <drorganvidez@us.es>
2022-05-31 17:30:08 +0200

    fix: Arregla algunas .env faltantes para Laradock

--- install_laradock.bat
+++ install_laradock.bat
@@ -5,7 +5,7 @@ COPY createdb.sql laradock\mysql\docker-entrypoint-initdb.d\createdb.sql
 cd laradock
 git pull origin master
 docker-compose up -d nginx mysql phpmyadmin redis workspace
-docker exec laradock_php-fpm-1 chown -R www-data:www-data /var/www/storage
+docker exec laradock_php-fpm_1 chown -R www-data:www-data /var/www/storage
 docker exec laradock_workspace_1 rm -f composer.lock
 docker exec laradock_workspace_1 composer install
 docker exec laradock_workspace_1 npm install
