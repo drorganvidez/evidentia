b22c5d199c5453575c5ca46f180ffe7499374b8a
gonmarmar5 <80246529+gonmarmar5@users.noreply.github.com>
2022-11-02 19:17:31 +0100

    Commit test
    A commit just to test that there are no errors pushing and pulling commits.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -129,7 +129,7 @@
                                     <h5 class="widget-user-desc text-left" style="margin-bottom: 0px">
                                         <i class="fas fa-calendar-day"></i>
                                         &nbsp;&nbsp;
-                                        Eventos programados
+                                        Eventos programados CAMBIO
                                     </h5>
 
                                 </div>
