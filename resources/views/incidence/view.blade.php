3253384478207d775f8d11c439df2a8de6d8d2f2
marrodgar62 <mario_plis@hotmail.com>
2022-11-12 19:26:14 +0100

    HIDDEN DELETE BUTTON ON AN INCIDENCE UNDER REVIEW - FIX: The delete button has been hidden when you enter in a in review incidence.

--- resources/views/incidence/view.blade.php
+++ resources/views/incidence/view.blade.php
@@ -39,7 +39,7 @@
 
                         @else
 
-                            @if(!\Carbon\Carbon::now()->gt(\Config::upload_incidences_timestamp()))
+                            @if($incidence->status != 'IN REVIEW' and !\Carbon\Carbon::now()->gt(\Config::upload_incidences_timestamp()))
                                 <x-buttonconfirm :id="$incidence->id" route="incidence.remove" title="¿Seguro?" description="Esto borrará la incidencia actual, las
                                                 ediciones anteriores <b>y todos los archivos adjuntos.</b>" type="REMOVE"/>
                             @endif
