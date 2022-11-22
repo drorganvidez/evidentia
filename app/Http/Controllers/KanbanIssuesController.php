85f4262555f3c6b03422977e7f93f5190fffcf85
Manueldm01 <manueldominguezmagdaleno@gmail.com>
2022-11-22 10:12:28 +0100

    borrar historial

---
+++ app/Http/Controllers/KanbanIssuesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exports\MyAttendeesExport;
+use App\Models\KanbanIssues;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Maatwebsite\Excel\Facades\Excel;
+
+class KanbanIssuesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('checkroles:STUDENT|COORDINATOR');
+    }
+
+    public function table()
+    {
+        $instance = \Instantiation::instance();
+        //$issues = KanbanIssues::where(['user_id' => Auth::id()])->get();
+
+        return view('kanban.table',
+            ['instance' => $instance]); //, 'issues' => $issues
+    }
+      
+
+}
---
+++ app/Models/KanbanIssues.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KanbanIssues extends Model
+{
+
+    protected $table="kanbanissues";
+
+    protected $fillable = [
+        'id', 'task', 'description', 'hours', 'user_id', 'comittee_id', 'type'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function comittee()
+    {
+        return $this->belongsTo('App\Models\Comittee');
+    }
+
+    public static function issues_to_do() {
+        return KanbanIssues::where('type','=', 'TODO')->get();
+    }
+
+    public static function issues_in_progress() {
+        return KanbanIssues::where('type','=', 'INPROGRESS')->get();
+    }
+
+    public static function issues_closed() {
+        return KanbanIssues::where('type','=', 'CLOSED')->get();
+    }
+
+}
