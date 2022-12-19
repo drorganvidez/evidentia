414ad626683f76d412df4f62c7a6d7a26b3119af
drorganvidez <davromorg@alum.us.es>
2022-12-20 00:02:34 +0100

    fix: Fixes dark mode for Laravel Vite

--- public/build/js/custom.js
+++ public/build/js/custom.js
@@ -12,6 +12,48 @@ function close_modal_autosaved()
 function dark_mode_toggle()
 {
 
+    let manifest_url = location.origin + '/build/manifest.json'
+
+    $.getJSON(manifest_url, function(manifest) {
+
+        // Icon
+        let icon_css_mode = $("#icon_css_mode");
+
+        // Get CSS filenames
+        let file_css_light_mode = location.origin + "/build/" + manifest['resources/sass/app_light_mode.scss']["file"];
+        let file_css_dark_mode = location.origin  + "/build/" + manifest['resources/sass/app_dark_mode.scss']["file"];
+
+        console.log(file_css_light_mode)
+        console.log(file_css_dark_mode)
+
+        // Change CSS style
+        let link_preload = $('link[rel=preload]')
+        let link_stylesheet = $('link[rel=stylesheet]')
+
+        if (link_preload.attr('href') === file_css_light_mode){
+            link_preload.attr('href', file_css_dark_mode)
+            link_stylesheet.attr('href', file_css_dark_mode)
+            icon_css_mode.attr('class', 'fe fe-sun');
+        } else {
+            link_preload.attr('href', file_css_light_mode)
+            link_stylesheet.attr('href', file_css_light_mode)
+            icon_css_mode.attr('class', 'fe fe-moon');
+        }
+
+        // Change logo
+        let logo_src = $("#logo_css").attr('src');
+        if(logo_src.includes("logo_dark.svg")){
+            logo_src = logo_src.replace('logo_dark.svg', 'logo_light.svg');
+        }else{
+            logo_src = logo_src.replace('logo_light.svg', 'logo_dark.svg');
+        }
+        $("#logo_css").attr('src', logo_src);
+
+
+
+    });
+
+    /*
     let href = $("#css_mode").attr('href');
     var icon_css_mode = $("#icon_css_mode");
 
@@ -39,6 +81,6 @@ function dark_mode_toggle()
         logo_src = logo_src.replace('logo_light.svg', 'logo_dark.svg');
     }
     $("#logo_css").attr('src', logo_src);
+    */
 
 }
-
