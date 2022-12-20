d809abb534b8b68abea69bbbd6f54a87d2ee1bae
David Romero <drorganvidez@us.es>
2022-12-20 11:25:32 +0100

    fix: Fixes installation on Windows plataforms

---
+++ .env.dev.bat
@@ -0,0 +1,4 @@
+set "DB_USERNAME=evidentia"
+set "DB_DATABASE=evidentia"
+set "DB_PORT=3306"
+set "DB_PASSWORD=secret"
--- devtools/install.bat
+++ devtools/install.bat
@@ -1,31 +1,42 @@
 @echo off
+
+rem we move to root folder
 cd ..
+
+rem download laradock repository
 git submodule update --init --recursive
 
-rem load environment variables
-copy .env.dev .env
-call .env
+rem set Laravel development environment
+copy .env.dev .env >nul
 
-rem changes environment
-copy .env.laradock laradock\.env
-sed -i "" "s/{{MYSQL_DATABASE}}/%DB_DATABASE%/g" laradock\.env
-sed -i "" "s/{{MYSQL_PASSWORD}}/%DB_PASSWORD%/g" laradock\.env
-sed -i "" "s/{{MYSQL_PORT}}/%DB_PORT%/g" laradock\.env
-sed -i "" "s/{{MYSQL_ROOT_PASSWORD}}/%DB_PASSWORD%/g" laradock\.env
+rem set Laradock environment
+copy .env.laradock laradock\.env >nul
+
+rem load environment variables
+call .env.dev.bat
+
+rem set dev environment variables
+
+rem Llama a la función y proporciona los parámetros necesarios
+call :refactorizar {{MYSQL_DATABASE}} %DB_DATABASE% laradock\.env
+call :refactorizar {{MYSQL_PORT}} %DB_PORT% laradock\.env
+call :refactorizar {{MYSQL_PASSWORD}} %DB_PASSWORD% laradock\.env
+call :refactorizar {{MYSQL_ROOT_PASSWORD}} %DB_PASSWORD% laradock\.env
 
 rem changes SQL entry point database
-copy entrypoint.sql start.sql
-sed -i "" "s/database_user/%DB_USERNAME%/g" start.sql
-sed -i "" "s/database_name/%DB_DATABASE%/g" start.sql
-sed -i "" "s/database_password/%DB_PASSWORD%/g" start.sql
-copy start.sql laradock\mysql\docker-entrypoint-initdb.d\createdb.sql
+copy entrypoint.sql start.sql >nul
+
+call :refactorizar database_user %DB_USERNAME% start.sql
+call :refactorizar database_name %DB_DATABASE% start.sql
+call :refactorizar database_password %DB_PASSWORD% start.sql
+
+copy start.sql laradock\mysql\docker-entrypoint-initdb.d\createdb.sql >nul
 del start.sql
 
 rem up and run containers
 cd laradock
 docker compose down
-rd /s /q "%userprofile%\.laradock\data\mysql"
-rd /s /q "%userprofile%\.laradock\data\redis"
+rd /s /q "%userprofile%\.laradock\data" 2>&1
 docker compose up -d --build nginx mysql phpmyadmin redis workspace
 docker exec laradock-php-fpm-1 chown -R www-data:www-data /var/www/storage
 docker exec laradock-workspace-1 del composer.lock
@@ -36,7 +47,7 @@ docker exec laradock-workspace-1 php artisan evidentia:start
 docker exec laradock-workspace-1 php artisan evidentia:instance
 docker exec laradock-workspace-1 php artisan key:generate
 
-echo
+echo.
 echo WELCOME TO
 echo .########.##.....##.####.########..########.##....##.########.####....###...;
 echo .##.......##.....##..##..##.....##.##.......###...##....##.....##....##.##..;
@@ -45,12 +56,33 @@ echo .######...##.....##..##..##.....##.######...##.##.##....##.....##..##.....#
 echo .##........##...##...##..##.....##.##.......##..####....##.....##..#########;
 echo .##.........##.##....##..##.....##.##.......##...###....##.....##..##.....##;
 echo .########....###....####.########..########.##....##....##....####.##.....##;
-echo
+echo.
 
 echo Open in a new browser:
 echo http://localhost
 
-echo
+echo.
 echo The installation has been completed successfully. Enjoy!
 
 pause
+
+rem Define la función y sus parámetros
+:refactorizar
+setlocal enabledelayedexpansion
+set "search=%~1"
+set "replace=%~2"
+set "textFile=%~3"
+
+rem Recorre cada línea del archivo especificado
+for /f "delims=" %%i in (%textFile%) do (
+    set "line=%%i"
+    set "line=!line:%search%=%replace%!"
+    echo !line!>>temp.txt
+)
+
+rem Reemplaza el archivo original con la versión modificada
+move /Y temp.txt %textFile% >nul
+endlocal
+goto :EOF
+
+
