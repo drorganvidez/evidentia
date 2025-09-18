09dddd39684a61b762b437b93f823d4188a8ed23
David Romero <drorganvidez@us.es>
2025-09-18 17:06:55 +0200

    fix: Fixes SSL/TLS connection between app and mysql

--- config/database.php
+++ config/database.php
@@ -59,7 +59,9 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_MODE') === 'DISABLED' ? false : true,
             ]) : [],
+
         ],
 
         'mariadb' => [
